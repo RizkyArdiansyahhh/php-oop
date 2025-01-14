e8c45e4b1b8d81302de901c0d42b84aaaf44b04f
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2025-01-14 16:02:53 +0700

    day 1 -> property

--- product.php
+++ product.php
@@ -3,12 +3,18 @@
 // Menjual Produk
 // Komik
 class Product{
-
+    public $title;
+    public $author;
+    public $publisher;
+    public $price;
 }
 
-$product1 = new Product();
-$product2 = new Product();
-var_dump($product1);
-var_dump($product2);
+$komik = new Product();
+$komik->title = "Naruto";
+$komik->author = "Rizky ardiansyah";
+$komik->publisher = "Rizky cinema";
+$komik->price = 300000;
+echo "Komik : $komik->title, $komik->author, $komik->publisher, $komik->price";
+
 
 ?>
