208309e6ec466a506312405c29120959426c761b
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2025-01-14 15:42:18 +0700

    day 1 -> class and object

---
+++ coba.php
@@ -0,0 +1,11 @@
+<?php 
+class Coba{
+
+}
+
+// Membuat instance dari kelas
+$coba1 = new Coba();
+$coba2 = new Coba();
+var_dump($coba1);
+var_dump($coba2);
+?>
---
+++ product.php
@@ -0,0 +1,14 @@
+<?php 
+
+// Menjual Produk
+// Komik
+class Product{
+
+}
+
+$product1 = new Product();
+$product2 = new Product();
+var_dump($product1);
+var_dump($product2);
+
+?>
