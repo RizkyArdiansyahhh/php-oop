39d0a3d01c3c5ab02647445a9283e4976e768966
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2025-01-16 09:36:36 +0700

    day 2 -> method

--- product.php
+++ product.php
@@ -7,6 +7,11 @@ class Product{
     public $author;
     public $publisher;
     public $price;
+
+
+    public function getProps(){
+        return "$this->title, $this->author";
+    }
 }
 
 $komik = new Product();
@@ -15,6 +20,16 @@ $komik->author = "Rizky ardiansyah";
 $komik->publisher = "Rizky cinema";
 $komik->price = 300000;
 echo "Komik : $komik->title, $komik->author, $komik->publisher, $komik->price";
+echo "<br>";
+echo "Komik : " . $komik->getProps();
+
+$game = new Product();
+$game->title = "Uncharted";
+$game->author = "Neil Druckmann";
+$game->publisher = "Sony Computer";
+$game->price = 250000;
+echo "<br>";
+echo "Game : " . $game->getProps();
 
 
 ?>
