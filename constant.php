1763142a67f377d10362cff0b36a1d966ecee820
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2025-01-18 23:41:35 +0700

    constant

---
+++ constant.php
@@ -0,0 +1,25 @@
+<?php 
+// define("NAMA", "Rizky ardiansyah"); //Tidak bisa di inisialisasi di dalam class
+// const UMUR = 20; //bisa di inisialisasi di dalam class
+
+
+
+// echo NAMA;
+// echo "<br>";
+// echo UMUR;
+
+
+// class Contoh{
+//     const NAMA = "Rizky";
+// }
+
+// echo Contoh::NAMA;
+
+echo __LINE__;
+echo "<br>";
+echo __FILE__;
+echo "<br>";
+echo __METHOD__;
+
+
+?>
